bf14453460923294b08d75dc9ebbc374c0c2c163
egrossetto <eugerosso22@hotmail.com>
2020-06-06 20:03:38 -0300

    jitsi added to project

--- tfm/src/App.js
+++ tfm/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Jitsi from 'react-jitsi'
+
+const roomName = 'my-super-secret-meeting-123e4567-e89b-12d3-a456-426655440000'
+const userFullName = 'Joseph Strawberry'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jitsi containerStyle={{ width: '100%', height: '1000px' }} roomName={roomName} displayName={userFullName} />
     </div>
   );
 }
