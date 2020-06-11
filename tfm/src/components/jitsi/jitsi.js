c8606c244b45fafa1ead62b0655b25084813d3b0
egrossetto <eugerosso22@hotmail.com>
2020-06-11 19:43:52 -0300

    add react import to jitsi

--- tfm/src/components/jitsi/jitsi.js
+++ tfm/src/components/jitsi/jitsi.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import Jitsi from 'react-jitsi'
+
 
 const roomName = 'my-super-secret-meeting-123e4567-e89b-12d3-a456-426655440000'
 const userFullName = 'Joseph Strawberry'
@@ -6,19 +8,19 @@ const userFullName = 'Joseph Strawberry'
 const JitsiVideo = () => {
     return(
         <Jitsi 
-        containerStyle={{ width: '100%', height: '1000px' }}
-        interfaceConfig={{    
-          SHOW_JITSI_WATERMARK: false,
-          TOOLBAR_BUTTONS: [
-            'microphone', 'camera', 'closedcaptions', 'desktop',
-            'fodeviceselection', 'hangup', 'profile', 'chat', 'recording', 'etherpad',
-            'filmstrip', 'invite', 'feedback',
-            'tileview', 'download', 'help', 'mute-everyone',
-            'security'
-          ],
-        }}
-        roomName={roomName}
-        displayName={userFullName} 
+          containerStyle={{ width: '100%', height: '1000px' }}
+          interfaceConfig={{    
+            SHOW_JITSI_WATERMARK: false,
+            TOOLBAR_BUTTONS: [
+              'microphone', 'camera', 'closedcaptions', 'desktop',
+              'fodeviceselection', 'hangup', 'profile', 'chat', 'recording', 'etherpad',
+              'filmstrip', 'invite', 'feedback',
+              'tileview', 'download', 'help', 'mute-everyone',
+              'security'
+            ],
+          }}
+          roomName={roomName}
+          displayName={userFullName} 
       />
     )
 }
