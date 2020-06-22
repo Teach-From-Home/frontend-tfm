ef47cafb0c252d28e3b2a547320c11b9e2e8538c
javiemgz <javiemgz@gmail.com>
2020-06-22 11:34:29 -0300

    ya estoy cansado

--- src/components/homework/studentsHomework.js
+++ src/components/homework/studentsHomework.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useState } from 'react'
-import { Grid, Box } from '@material-ui/core'
+import { Grid, Box, CircularProgress, Typography } from '@material-ui/core'
 import { UserContext } from '../../userContext';
 import HomeworkService from '../../services/homeworkService';
 import StudentsHomeworkCard from './studentsHomeworkCard';
@@ -8,23 +8,24 @@ import SnackbarOpen from "../snackbar/snackbar";
 export default function StudentsHomework() {
 
     const [homeworks, setHomeworks] = useState([]);
+    const [loading, setloading] = useState(false)
     const [snackbar, setSnackbar] = useState({
         open: false,
         message: "",
         severity: "success",
     });
-    
-    const {user} = useContext(UserContext);
+
+    const { user } = useContext(UserContext);
 
     const homeworkService = new HomeworkService();
 
     const closeSnackbar = (event, reason) => {
         if (reason === "clickaway") {
-          return;
+            return;
         }
         setSnackbar({
-          ...snackbar,
-          open: false,
+            ...snackbar,
+            open: false,
         });
     };
 
@@ -34,29 +35,41 @@ export default function StudentsHomework() {
     }, []);
 
     const getStudentsHomework = async () => {
-        try {
-            let hs = await homeworkService.getStudentsHomework(user.selectedHomework.id, user.id);
-            setHomeworks(hs);            
-        } catch (error) {
-            
-        }
+        setloading(true)
+        homeworkService.getStudentsHomework(user.selectedHomework.id, user.id)
+            .then((hs) => {
+                setloading(false)
+                setHomeworks(hs);
+            })
+            .catch(() => setloading(false))
+
     }
 
     return (
         <div>
-            <Grid container>
-                    {
-                        homeworks ?
-                        homeworks.map( h => {
-                            return (
-                                <Box m={2}>
-                                    <StudentsHomeworkCard setSnackbar={setSnackbar} homework={h} key={h.id}/>
-                                </Box>
-                            )
-                        })
+            <Grid 
+            direction='column' 
+            alignItems='center' 
+            justify='center'
+            container>
+                {
+                    !loading ?
+                        <span>
+                            {homeworks.map(h => {
+                                return (
+                                    <Box m={2}>
+                                        <StudentsHomeworkCard setSnackbar={setSnackbar} homework={h} key={h.id} />
+                                    </Box>
+                                )
+                            })}
+                            {homeworks.length === 0 ?
+                                <Typography variant="h2" style={{ color: '#636363', marginTop: '100px' }} > No hay tareas subidas por los alumnos</Typography>
+
+                                : ""}
+                        </span>
                         :
-                        <div></div>
-                    }
+                        <CircularProgress size={100} style={{ color: '#636363', marginTop: '100px' }} />
+                }
             </Grid>
             <SnackbarOpen
                 open={snackbar.open}
